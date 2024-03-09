50cccd2c63e93a2d7ce13252bda070d0887e4e07
Leogobbi24 <131538517+Leogobbi24@users.noreply.github.com>
2024-03-09 12:05:36 +0100

    migliorie dettaglio news

--- news-detail.php
+++ news-detail.php
@@ -16,11 +16,9 @@ if(!$result || $result->num_rows==0){
 <div class="rts-blog-details-area rts-section-gap">
     <div class="container">
         <div class="row">
-            <div class="col-xl-10 offset-xl-1 col-lg-10 offset-lg-1 col-md-12 col-sm-12 col-12">
+            <div class="col-xl-8 offset-xl-2 col-lg-8 offset-lg-2 col-md-12 col-sm-12 col-12">
                 <div class="blog-single-post-listing details mb--0">
-                    <div class="thumbnail">
-                        <img src="../assets/images/upload/<?php echo $news['immagine']?>" alt="<?php echo $news['immagine']?>">
-                    </div>
+                    <img src="../assets/images/upload/<?php echo $news['immagine']?>" alt="<?php echo $news['immagine']?>">
                     <div class="blog-listing-content">
                         <div class="user-info">
                             <div class="single">
@@ -32,6 +30,45 @@ if(!$result || $result->num_rows==0){
                         <p class="disc para-1 mb--15">
                             <?php echo $news['descrizione_dettagliata']?>
                         </p>
+                        <div class="row mt--40 gallery">
+                        	<?php if($news['immagine2']){ ?>
+				            <div class="col-lg-6 col-6">
+				                <a href="../assets/images/upload/<?php echo $news['immagine2']?>">
+				                    <div class="image" style="background-image:url('../assets/images/upload/<?php echo $news['immagine2']?>');"></div>
+				                </a>
+				            </div>
+				            <?php if($news['immagine3']){ ?>
+				            <div class="col-lg-6 col-6">
+				                <a href="../assets/images/upload/<?php echo $news['immagine3']?>">
+				                    <div class="image" style="background-image:url('../assets/images/upload/<?php echo $news['immagine3']?>');"></div>
+				                </a>
+				            </div>
+				            <?php if($news['immagine4']){ ?>
+				            <div class="col-lg-6 col-6">
+				                <a href="../assets/images/upload/<?php echo $news['immagine4']?>">
+				                    <div class="image" style="background-image:url('../assets/images/upload/<?php echo $news['immagine4']?>');"></div>
+				                </a>
+				            </div>
+				            <?php if($news['immagine5']){ ?>
+				            <div class="col-lg-6 col-6">
+				                <a href="../assets/images/upload/<?php echo $news['immagine5']?>">
+				                    <div class="image" style="background-image:url('../assets/images/upload/<?php echo $news['immagine5']?>');"></div>
+				                </a>
+				            </div>
+				            <?php if($news['immagine6']){ ?>
+				            <div class="col-lg-6 col-6">
+				                <a href="../assets/images/upload/<?php echo $news['immagine6']?>">
+				                    <div class="image" style="background-image:url('../assets/images/upload/<?php echo $news['immagine6']?>');"></div>
+				                </a>
+				            </div>
+				            <?php if($news['immagine7']){ ?>
+				            <div class="col-lg-6 col-6">
+				                <a href="../assets/images/upload/<?php echo $news['immagine7']?>">
+				                    <div class="image" style="background-image:url('../assets/images/upload/<?php echo $news['immagine7']?>');"></div>
+				                </a>
+				            </div>
+				            <?php } ?>
+				        </div>
                     </div>
                 </div>
             </div>
